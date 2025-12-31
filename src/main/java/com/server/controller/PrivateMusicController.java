ea64bb4ad9f95fa34782451383dc9d889648e069
程文楷 <17752035@gitlab.com>
2025-12-31 11:13:35 +0800

    updated range download

--- src/main/java/com/server/controller/PrivateMusicController.java
+++ src/main/java/com/server/controller/PrivateMusicController.java
@@ -5,7 +5,9 @@ import com.server.service.CloudMusicService;
 import com.server.entity.UserPrincipal;
 import jakarta.annotation.Resource;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -40,12 +42,32 @@ public class PrivateMusicController {
     }
 
     @GetMapping("/download/{id}")
-    public ResponseEntity<InputStreamResource> download(
+    public ResponseEntity<ResourceRegion> download(
             @PathVariable String id,
             @AuthenticationPrincipal UserPrincipal user,
-            @RequestHeader(value = "Range", required = false) String rangeHeader
+            @RequestHeader HttpHeaders headers
     ) throws IOException {
+
         File file = cloudMusicService.getFile(id, user.getId());
-        return buildRangeResponse(file, rangeHeader);
+        UrlResource resource = new UrlResource(file.toURI());
+
+        long contentLength = resource.contentLength();
+
+        ResourceRegion region;
+
+        if (headers.getRange().isEmpty()) {
+            region = new ResourceRegion(resource, 0, contentLength);
+        } else {
+            HttpRange range = headers.getRange().get(0);
+            long start = range.getRangeStart(contentLength);
+            long end = range.getRangeEnd(contentLength);
+            region = new ResourceRegion(resource, start, end - start + 1);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .header(HttpHeaders.ACCEPT_RANGES, "bytes")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(region);
     }
 }
